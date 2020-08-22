8c38ef45c6e6b61654220e9dce3df150d27afa4a
C1297675 Thiago Bernardo Fernandes Barbosa <c1297675@interno.bb.com.br>
2020-08-22 10:43:28 -0300

    Início da refatoração para usar reactive forms

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,5 +1,5 @@
 import { AfterContentInit, Component, ContentChild, Input, OnInit } from '@angular/core';
-import { NgModel } from '@angular/forms';
+import { FormControlName, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'mt-input-container',
@@ -13,6 +13,7 @@ export class InputComponent implements OnInit, AfterContentInit {
   input: any;
 
   @ContentChild(NgModel) model: NgModel;
+  @ContentChild(FormControlName) control: FormControlName;
 
   constructor() { }
 
@@ -20,10 +21,10 @@ export class InputComponent implements OnInit, AfterContentInit {
   }
 
   ngAfterContentInit() {
-    this.input = this.model;
+    this.input = this.model || this.control;
 
     if (this.input === undefined) {
-      throw new Error('Este componente precisa ser usado com a diretiva ngModel');
+      throw new Error('Este componente precisa ser usado com a diretiva ngModel ou formControlName');
     }
   }
 
