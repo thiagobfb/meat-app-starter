9ad82827701cb03c6abc3c592218cb838e541f60
C1297675 Thiago Bernardo Fernandes Barbosa <c1297675@interno.bb.com.br>
2020-09-16 16:53:54 -0300

    Finalização da implementação de busca de restaurantes

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Restaurant } from './restaurant/restaurant.model';
 import { RestaurantsService } from './restaurants.service';
 import { animate, state, style, transition, trigger } from '@angular/animations';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'mt-restaurants',
@@ -24,12 +26,26 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
 export class RestaurantsComponent implements OnInit {
 
   searchBarState = 'hidden';
-
   restaurants: Restaurant[];
+  searchForm: FormGroup;
+  searchControl: FormControl;
 
-  constructor(private restaurantsService: RestaurantsService) { }
+  constructor(private restaurantsService: RestaurantsService,
+              private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.searchControl = this.formBuilder.control('');
+    this.searchForm = this.formBuilder.group({
+      searchControl: this.searchControl
+    });
+
+    this.searchControl.valueChanges
+      .debounceTime(500)
+      .distinctUntilChanged()
+      .switchMap(searchTerm => this.restaurantsService
+        .restaurants(searchTerm)
+        .catch(error => Observable.from([])))
+      .subscribe(restaurants => this.restaurants = restaurants);
     this.restaurantsService.restaurants().subscribe(restaurants => this.restaurants = restaurants);
   }
 
